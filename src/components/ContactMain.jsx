eab3b566338a64a4aa03ca2ad9a4699a061cf6e9
ikeshav26 <keshavgilhotra4@gmail.com>
2025-04-20 14:41:50 +0530

    contact changes

--- src/components/ContactMain.jsx
+++ src/components/ContactMain.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 
 const ContactMain = () => {
-    const notify=()=>{
+    const notify = (e) => {
+        e.preventDefault()
+
         toast.success("Your message has been sent successfully")
     }
+
     return (
         <div className=''>
             <div className='flex flex-col justify-center items-center h-screen gap-24 '>
@@ -12,15 +15,43 @@ const ContactMain = () => {
                     <h1 className='text-[#29A399] text-4xl font-semibold left-15 absolute '>Contact Volto</h1>
                 </div>
                 <div className='w-[70vw] md:w-[60vw] lg:[50vw]'>
-                    <fieldset className="fieldset flex flex-col gap-5 w-full">
-                        <input type="text" required className="input  w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold" placeholder="Your name" />
-                        <input type="email" required className="input w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold" placeholder="Email" />
-                        <input type="text" required className="input w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold" placeholder="Tell us what you ned help with" />
-                        <input type="text" required className="input w-full h-[120px] text-[#0B78D0] bg-[#F2F4F7] font-bold placeholder:top-0 placeholder:absolute placeholder:py-4" placeholder="Give us more details" />
+                    <form onSubmit={notify} className="fieldset flex flex-col gap-5 w-full">
+                        <input
+                            type="text"
+                            name="name"
+                            required
+                            className="input w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold"
+                            placeholder="Your name"
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            className="input w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold"
+                            placeholder="Email"
+                        />
+                        <input
+                            type="text"
+                            name="subject"
+                            required
+                            className="input w-full h-[48px] text-[#0B78D0] bg-[#F2F4F7] font-bold"
+                            placeholder="Tell us what you need help with"
+                        />
+                        <textarea
+                            name="details"
+                            required
+                            className="input w-full h-[120px] text-[#0B78D0] bg-[#F2F4F7] font-bold placeholder:top-0 placeholder:absolute placeholder:py-4"
+                            placeholder="Give us more details"
+                        />
                         <div className='flex justify-end'>
-                        <button onClick={()=>{notify()}} className='bg-[#0B78D0] hover:opacity-80 cursor-pointer w-16 font-semibold px-3 py-2 rounded-lg mt-4'>Send</button>
+                            <button
+                                type="submit"
+                                className='bg-[#0B78D0] hover:opacity-80 cursor-pointer w-16 font-semibold px-3 py-2 rounded-lg mt-4'
+                            >
+                                Send
+                            </button>
                         </div>
-                    </fieldset>
+                    </form>
                 </div>
             </div>
         </div>
